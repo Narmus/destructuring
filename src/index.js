5692e684c2580db627a4fd0456b6718c92fd1f11
Arun M S <narmusgaming@gmail.com>
2022-10-24 21:42:04 +0530

    Destructuring tutorial

---
+++ src/index.js
@@ -0,0 +1,42 @@
+// CHALLENGE: uncomment the code below and see the car stats rendered
+import React from "react";
+import ReactDOM from "react-dom";
+import cars from "./practice";
+
+const [honda, tesla] = cars;
+
+const {
+  coloursByPopularity: [hondaTopColour],
+  speedStats: { topSpeed: hondaTopSpeed },
+} = honda;
+
+const {
+  coloursByPopularity: [teslaTopColour],
+  speedStats: { topSpeed: teslaTopSpeed },
+} = tesla;
+
+console.log(hondaTopSpeed, hondaTopColour);
+console.log(teslaTopSpeed, teslaTopColour);
+
+ReactDOM.render(
+  <table>
+    <tbody>
+      <tr>
+        <th>Brand</th>
+        <th>Top Speed</th>
+        <th>Top Color</th>
+      </tr>
+      <tr>
+        <td>{tesla.model}</td>
+        <td>{teslaTopSpeed}</td>
+        <td>{teslaTopColour}</td>
+      </tr>
+      <tr>
+        <td>{honda.model}</td>
+        <td>{hondaTopSpeed}</td>
+        <td>{hondaTopColour}</td>
+      </tr>
+    </tbody>
+  </table>,
+  document.getElementById("root")
+);
